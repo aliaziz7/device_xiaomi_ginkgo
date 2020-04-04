caa45c5ae7a83d19686fb68b472f9a1f7987d415
Aoihara <veronicadisa6@gmail.com>
2020-04-04 23:09:48 +0700

    fp from crosshatch

--- init/init_ginkgo.cpp
+++ init/init_ginkgo.cpp
@@ -58,6 +58,6 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 void vendor_load_properties()
 {
     // fingerprint
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Redmi/begonia/begonia:10/QP1A.190711.020/V11.0.1.0.QGGIDXM:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/crosshatch/crosshatch:10/QQ2A.200305.002/6138846:user/release-keys");
     property_override("vendor.audio.adm.buffering.ms", "6");
 }
